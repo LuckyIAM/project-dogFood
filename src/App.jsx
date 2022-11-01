2d3e45aa050e212f5ce2459c9c5347b40e20aef8
LuckyIAM <zavalisca.elena@gmail.com>
2022-11-01 20:55:05 +0300

    not work

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 
-import React from "react";
+import React, {useState} from "react";
 import Card from "./components/Card";
 import Header from "./components/Header";
 
 import data from "./assets/data.json"
-class App extends React.Component{
-    render(){
-        return(
-            <div className="wrapper">
-                <Header/>
-                <div className="cards-container">
-                    {/* <Card/> */}
-                    {data.map((d,i) =><Card 
-                    key = {i}
-                    img = {d.picture}
-                    text = {d.name}
-                    price = {d.price}
-                    />)}
-                </div>
+const App = () => {
+    const [goods,setGoods] = useState(data);
+
+    return(
+        <div className="wrapper">
+            <Header products={goods}/>
+            <div className="cards-container">
+                {/* <Card/> */}
+                {goods.map((d,i) =><Card 
+                key = {i}
+                img = {d.picture}
+                text = {d.name}
+                price = {d.price}
+                />)}
             </div>
-        )
-    }
+        </div>
+    )
 }
 export default App;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css";
 import Logo from "../Logo";
+import {ReactComponent as FavIcon} from "./img/ic-favorites.svg";
+import {ReactComponent as CartIcon} from "./img/ic-cart.svg";
+import {ReactComponent as ProfileIcon} from "./img/ic-profile.svg";
+import { set } from "core-js/core/dict";
 
-
-export default() => {
-    return <header>
-        <div className="logo"/>
-        <input type="search"/>
-        <nav>
-            <a href="">Favorite</a>
-            <a href="">Cart</a>
-            <a href="">Profile</a>
-        </nav>
-    </header>
+export default({products}) => {
+    const [text,changeText] =useState('Рога');
+    const [cnt, setCnt] = useState(0);
+    const handler = e=>{
+        changeText(e.target.value);
+        const result = products.filter(el => el.name.toLowerCase().search(e.target.value.toLowerCase()) !== -1);
+        console.log(result);
+        setCnt(result.length)
+    }
+    return <>
+        <header>
+            <Logo/>
+            <input type="search" value={text} onChange={handler}/>
+            <nav>
+                <a href=""><FavIcon/></a>
+                <a href=""><CartIcon/></a>
+                <a href=""><ProfileIcon/></a>
+            </nav>
+        </header>
+        <div>
+            {text ? `По запросу ${text} наидено ${cnt} позиции` : `Пойск ...`}
+        </div>
+    </>
 }
