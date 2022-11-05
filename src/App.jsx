9adff72931e3bf31dfe6a6afda860bda92c20777
LuckyIAM <zavalisca.elena@gmail.com>
2022-11-05 19:58:15 +0300

    dont work2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
-
 import React, {useState} from "react";
-import Catalog from "./pages"
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Col, Container,Row} from "react-bootstrap";
 
 
-const App () =>{
-    return <Catalog/>
+const App = () =>{
+    const st = {
+        height: "50px",
+        backgroundColor: "silver",
+        border: "1px solid white"
+    }
+    return <Container style = {{height: "900px", backgroundColor: "darkorchid"}}>
+            <Row>
+                <Col style={st}/>
+                <Col style={st}/>
+                <Col style={st}/>
+            </Row>
+        </Container>
 }
 
 export default App;
